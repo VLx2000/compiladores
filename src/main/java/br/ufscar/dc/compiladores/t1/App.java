fa29eda8992f3b1006b8a1993d79c2a84dc6df63
Lucas D <lucasdomingues2000@hotmail.com>
2022-06-19 11:35:01 -0300

    Exibe msg de erro para simbolos desconhecidos

--- src/main/java/br/ufscar/dc/compiladores/t1/App.java
+++ src/main/java/br/ufscar/dc/compiladores/t1/App.java
@@ -18,8 +18,21 @@ public class App {
             CharStream cs = CharStreams.fromFileName(entrada);
             t1Lexer lex = new t1Lexer(cs);
             Token t = null;
-            while ((t = lex.nextToken()).getType() != Token.EOF) {
-                pw.write("<" + "\'" + t.getText()  + "\'" + "," + t1Lexer.VOCABULARY.getDisplayName(t.getType()) + ">\n");
+            int erro = 0,line = 0;
+            String Regra ="",token ="";
+            while ((t = lex.nextToken()).getType() != Token.EOF && erro != 1) {
+                line = lex.getLine();
+                Regra =  t1Lexer.VOCABULARY.getDisplayName(t.getType());
+                if(!Regra.equals("ERRO")){
+                    pw.write("<" + "\'" + t.getText()  + "\'" + "," + Regra + ">\n");
+                }else{
+                    token = t.getText();
+                    erro = 1;
+                }
+                
+            }
+            if(erro == 1){
+                pw.write("Linha "+ line + ": " +token+ " - simbolo nao identificado\n");
             }
             pw.close();
         } catch (IOException ex) {
